3090d8022edcfe531ba94117260544d4fa9f2ffe
aliso4ka <gulyayevaalisa@gmail.com>
2024-10-21 10:58:23 +0200

    fixed

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,17 +1,31 @@
- import style from "./Options.module.css"
+import style from './Options.module.css';
 
+export default function Options({ updateFeedback, totalFeedback }) {
+  const handleReset = () => {
+    updateFeedback('reset');
+  };
 
-export default function Options({ updateFeedback, totalFeedback }) {  // Получаем updateFeedback через {}
-  
-    const handleReset = () => {
-        updateFeedback('reset'); 
-  }
-    return (
+  return (
     <>
-      <button onClick={() => updateFeedback('good')} className={style.good}>Good</button>
-      <button onClick={() => updateFeedback('neutral')} className={style.neutral}>Neutral</button>
-    <button onClick={() => updateFeedback('bad')} className={style.bad}>Bad</button>
-    {totalFeedback>0 ? <button onClick={handleReset} className={style.reset}>Reset</button> : <p></p>}       
+      <button onClick={() => updateFeedback('good')} className={style.good}>
+        Good
+      </button>
+      <button
+        onClick={() => updateFeedback('neutral')}
+        className={style.neutral}
+      >
+        Neutral
+      </button>
+      <button onClick={() => updateFeedback('bad')} className={style.bad}>
+        Bad
+      </button>
+      {totalFeedback > 0 ? (
+        <button onClick={handleReset} className={style.reset}>
+          Reset
+        </button>
+      ) : (
+        <p></p>
+      )}
     </>
   );
 }
